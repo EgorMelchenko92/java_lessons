3b3fe7ebca390c029c5cb797c8fb1a2e403dd334
EgorMelchenko92 <desay19921223@gmail.com>
2019-07-06 13:43:03 +0300

    Second homework

--- Main.java
+++ Main.java
@@ -1,24 +1,98 @@
 package com.melchenko;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter count potatos:");
-        int countPotatos = scanner.nextInt();
+            while (true) {
 
-        System.out.println("Please enter count package:");
-        int countPackage = scanner.nextInt();
 
-        int countPotatosIntoPackage;
+                    int number = 1;
+                    do {
 
-        countPotatosIntoPackage=countPotatos/countPackage;
-        System.out.println("Count potatos into package: " + countPotatosIntoPackage);
-        System.out.println("Reminder potatos: " + (countPotatos-(countPackage*countPotatosIntoPackage)));
+                        System.out.println(" ");
+                        System.out.println("___________________________________________________");
+                        System.out.println("Number iteration: " + number);
+                        number++;
+                        System.out.println("___________________________________________________");
+
+                        int maxCountPackage = 10;
+
+                        Scanner scanner = new Scanner(System.in);
+
+                        System.out.println(" ");
+
+                        System.out.println("Please enter count potatos:");
+                        int countPotatos = scanner.nextInt();
+
+                        if (!checkValue(countPotatos)) {
+                            System.out.println("This is fiasko brother");
+                        } else {
+
+                            System.out.println("Please enter size package:");
+                            int sizePackage = scanner.nextInt();
+
+                            if (!checkValue(sizePackage)) {
+                                System.out.println("This is fiasko brother");
+                            } else {
+
+                                int countPackage;
+
+                                countPackage = countPotatos / sizePackage;
+
+                                int countNeedPackage;
+                                int countLetters = 33;
+
+                                countNeedPackage = countPackage - maxCountPackage;
+
+                                if (countPackage > maxCountPackage) {
+                                    System.out.println("This is fiasco brother");
+                                    System.out.println("Exceeding the maximum number of bags");
+                                    System.out.println("You are need still package: " + countNeedPackage);
+                                } else if (countPackage <= maxCountPackage) {
+                                    System.out.println("This is good");
+                                }
+
+                                System.out.println("Count package: " + countPackage);
+                                System.out.println("Reminder potatos: " + (countPotatos - (sizePackage * countPackage)));
+
+
+                                System.out.println("___________________________________________________");
+                                System.out.println("Potatoes packed in the following bags:");
+                                System.out.println(" ");
+
+                                //алфавит
+                                ArrayList<Character> alphabet = new ArrayList<Character>();
+                                for (int i = 0; i < 34; i++) {
+                                    alphabet.add((char) ('а' + i));
+
+                                    if (countPackage <= i ) {
+                                        for (int j = 1; i <= countPackage && j <= i; j++) {
+                                            System.out.print("[" + j + alphabet.get(j - 1) + "]" + " ");
+                                        }
+                                    }
+                                } if (countPackage > 33) {
+                                    System.out.println("___________________________________________________");
+                                    System.out.println("It's a brother fiasco, the letters are over ");
+                                    System.out.println("You need more letters: " + (countPackage-countLetters));
+                                    System.out.println(" ");
+
+                                }
+                            }
+
+                        }
+                    } while (number <= 3);
+                    break;
+        }
+        System.out.println("Completion of the iteration");
+    }
+
+
+    public static boolean checkValue(int inputValue) {
+        return inputValue > 0 && inputValue < 1001;
     }
 }
-
